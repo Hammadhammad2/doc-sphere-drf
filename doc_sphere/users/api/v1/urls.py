b23570fe5efccb3c60dc34aaff77308f81eaafc6
Hammad Naeem <hammad.kinectro@gmail.com>
2026-03-30 15:08:40 +0500

    Update delete account view to align with RESTful practices
    - Renamed `DeleteAccountAPIView` to `DeleteAccountDestroyAPIView` for consistency with RESTful naming conventions.
    - Adjusted import statements in `urls.py` to reflect the updated view name.

--- doc_sphere/users/api/v1/urls.py
+++ doc_sphere/users/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 
-from .views import DeleteAccountAPIView, GoogleSocialLoginView
+from .views import DeleteAccountDestroyAPIView, GoogleSocialLoginView
 
 
 urlpatterns = [
     path("registration/", include("dj_rest_auth.registration.urls")),
     path("registration/google/", GoogleSocialLoginView.as_view(), name="google_login"),
-    path("delete-account/", DeleteAccountAPIView.as_view(), name="delete_account"),
+    path("delete-account/", DeleteAccountDestroyAPIView.as_view(), name="delete_account"),
     path("", include("dj_rest_auth.urls")),
 ]
